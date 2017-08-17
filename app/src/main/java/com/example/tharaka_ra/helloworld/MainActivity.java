98ece737c311d22471e7d18e1ed975865be8c949
Tharaka Ranatunga <tharaka.14@cse.mrt.ac.lk>
2017-08-17 13:41:37 +0530

    basic working calculator

--- app/src/main/java/com/example/tharaka_ra/helloworld/MainActivity.java
+++ app/src/main/java/com/example/tharaka_ra/helloworld/MainActivity.java
@@ -14,12 +14,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button btnAdd = (Button) findViewById(R.id.addition);
+        Button btnSubstract = (Button) findViewById(R.id.substraction);
+        Button btnMultiply = (Button) findViewById(R.id.multiplication);
+        Button btnDivide = (Button) findViewById(R.id.division);
+        final TextView first = (TextView) findViewById(R.id.first);
+        final TextView last = (TextView) findViewById(R.id.last);
+
+
 
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                double firstNumber = Double.parseDouble(first.getText().toString());
+                double lastNumber = Double.parseDouble(last.getText().toString());
                 TextView result = (TextView) findViewById(R.id.result);
-
+                result.setText(Double.toString(firstNumber+lastNumber));
+            }
+        });
+        btnSubstract.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double firstNumber = Double.parseDouble(first.getText().toString());
+                double lastNumber = Double.parseDouble(last.getText().toString());
+                TextView result = (TextView) findViewById(R.id.result);
+                result.setText(Double.toString(firstNumber-lastNumber));
+            }
+        });
+        btnMultiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double firstNumber = Double.parseDouble(first.getText().toString());
+                double lastNumber = Double.parseDouble(last.getText().toString());
+                TextView result = (TextView) findViewById(R.id.result);
+                result.setText(Double.toString(firstNumber*lastNumber));
+            }
+        });
+        btnDivide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double firstNumber = Double.parseDouble(first.getText().toString());
+                double lastNumber = Double.parseDouble(last.getText().toString());
+                TextView result = (TextView) findViewById(R.id.result);
+                result.setText(Double.toString(firstNumber/lastNumber));
             }
         });
 
